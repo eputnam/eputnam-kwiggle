7d92966f5423d561f766e82851e19be3d431d170
Eric Putnam <putnam.eric@gmail.com>
2024-03-14 13:55:27 -0700

    change service for a real change this time

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,7 @@ define kwiggle::service {
   file { 'service file':
     path    => "/etc/kwiggle.service",
     ensure  => present,
-    content => "my different cool service is great and awesome, especially after everything is in the right place",
+    content => "my different cool service is great and awesome, especially after everything is in the right place. now for a real repro",
   }
 
 }
