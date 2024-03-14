8549406b8f7468206fa94360884c9e99d9f03c1c
Eric Putnam <putnam.eric@gmail.com>
2024-03-14 11:05:13 -0700

    change service content

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,7 +2,7 @@ define kwiggle::service {
   file { 'service file':
     path    => "/etc/kwiggle.service",
     ensure  => present,
-    content => "my cool service",
+    content => "my different cool service",
   }
 
 }
